fea93dbee18ec970beb43e3a582eb3db07aa9635
bradley.r.blankenship <bradley.r.blankenship@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-01-05 20:46:06 +0000

    Committed the basic views and perspectives require for the BridgIt layout. Next, need to work on getting the layout correct, followed by adding command and command handler stubs to the layout controls.

---
+++ BridgIt/src/edu/rit/se/bridgit/edit/views/CodeView.java
@@ -0,0 +1,24 @@
+package edu.rit.se.bridgit.edit.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class CodeView extends ViewPart {
+
+	public CodeView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BridgIt/src/edu/rit/se/bridgit/edit/views/TypesView.java
@@ -0,0 +1,24 @@
+package edu.rit.se.bridgit.edit.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class TypesView extends ViewPart {
+
+	public TypesView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
