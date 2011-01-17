776194a55b1a728c368bbfaf482ffcdb1f0e63f3
bradley.r.blankenship <bradley.r.blankenship@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-01-17 22:49:47 +0000

    updated the layouts for the execution screen. Also began work on changing the commands for adding them to the "coolbar".

---
+++ BridgIt/src/edu/rit/se/bridgit/views/WorldView.java
@@ -0,0 +1,24 @@
+package edu.rit.se.bridgit.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class WorldView extends ViewPart {
+
+	public WorldView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
