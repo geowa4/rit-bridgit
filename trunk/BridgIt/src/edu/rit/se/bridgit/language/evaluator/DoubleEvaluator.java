a0749ff4497bcd0edee42764e5b658ed742a5f04
G.W.Adams.IV <G.W.Adams.IV@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-01-21 18:49:49 +0000

    Two new term evals

---
+++ trunk/BridgIt/src/edu/rit/se/bridgit/language/evaluator/DoubleEvaluator.java
@@ -0,0 +1,26 @@
+package edu.rit.se.bridgit.language.evaluator;
+
+import edu.rit.se.bridgit.language.model.InvalidTypeException;
+import edu.rit.se.bridgit.language.model.Type;
+
+public class DoubleEvaluator extends Evaluator {
+
+	private Double value;
+	
+	public DoubleEvaluator(Double value) {
+		super();
+		this.value = value;
+	}
+
+	@Override
+	public Type evaluate() {
+		return new Type(value);
+	}
+
+	@Override
+	protected void validateType(Type t) throws InvalidTypeException {
+		if(t.getType().equals(Integer.class))
+			throw new InvalidTypeException(t.getType(), "Double");
+	}
+
+}
---
+++ trunk/BridgIt/src/edu/rit/se/bridgit/language/evaluator/StringEvaluator.java
@@ -0,0 +1,26 @@
+package edu.rit.se.bridgit.language.evaluator;
+
+import edu.rit.se.bridgit.language.model.InvalidTypeException;
+import edu.rit.se.bridgit.language.model.Type;
+
+public class StringEvaluator extends Evaluator {
+
+	private String value;
+	
+	public StringEvaluator(String value) {
+		super();
+		this.value = value;
+	}
+
+	@Override
+	public Type evaluate() {
+		return new Type(value);
+	}
+
+	@Override
+	protected void validateType(Type t) throws InvalidTypeException {
+		if(t.getType().equals(String.class))
+			throw new InvalidTypeException(t.getType(), "String");
+	}
+
+}
