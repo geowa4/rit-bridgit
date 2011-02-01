11b588b761a4bd3daa0dbe920a66f977aa329cf7
G.W.Adams.IV <G.W.Adams.IV@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-01 04:47:58 +0000

    Moved ConditionalBlockPair out so it can be used in multiple classes. Fixed WhileEvaluator to not just be another IfEvaluator.

--- BridgIt/src/edu/rit/se/bridgit/language/evaluator/IfEvaluator.java
+++ BridgIt/src/edu/rit/se/bridgit/language/evaluator/IfEvaluator.java
@@ -45,26 +45,3 @@ public class IfEvaluator extends Evaluator
 		}
 	}
 }
-
-class ConditionalBlockPair
-{
-	private Evaluator block;
-	private Evaluator conditional;
-
-	public Evaluator getConditional() 
-	{
-		return conditional;
-	}
-	public void setConditional(Evaluator conditional) 
-	{
-		this.conditional = conditional;
-	}
-	public Evaluator getBlock() 
-	{
-		return block;
-	}
-	public void setBlock(Evaluator block) 
-	{
-		this.block = block;
-	}
-}
--- BridgIt/src/edu/rit/se/bridgit/language/evaluator/WhileEvaluator.java
+++ BridgIt/src/edu/rit/se/bridgit/language/evaluator/WhileEvaluator.java
@@ -1,40 +1,39 @@
 package edu.rit.se.bridgit.language.evaluator;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import edu.rit.se.bridgit.language.model.InvalidTypeException;
 import edu.rit.se.bridgit.language.model.NameConflictException;
 import edu.rit.se.bridgit.language.model.Type;
 
 public class WhileEvaluator extends Evaluator
 {
+	private ConditionalBlockPair pair;
 	
-	private List<ConditionalBlockPair> ladder = 
-		new LinkedList<ConditionalBlockPair>();
-	
-	public boolean addConditional(Evaluator conditional, Evaluator block)
+	public WhileEvaluator()
 	{
-		ConditionalBlockPair pair = new ConditionalBlockPair();
+		pair = new ConditionalBlockPair();
+	}
+	
+	public void setConditional(Evaluator conditional, Evaluator block)
+	{
 		pair.setConditional(conditional);
 		pair.setBlock(block);
-		return ladder.add(pair);
 	}
 
 	@Override
 	public Type evaluate(Scope scope) throws InvalidTypeException, NameConflictException {
-		scope = new Scope(scope);
-		for(ConditionalBlockPair pair : ladder)
+		while(executeConditional(scope))
 		{
-			Type val = pair.getConditional().evaluate(scope);
-			validateType(val);
-			if((Boolean) val.getValue())
-			{
-				pair.getBlock().evaluate(scope);
-				return val;
-			}
+			pair.getBlock().evaluate(scope);
 		}
 		return null;
+	}
+	
+	private Boolean executeConditional(Scope scope) 
+	throws InvalidTypeException, NameConflictException
+	{
+		Type ret = pair.getConditional().evaluate(scope);
+		validateType(ret);
+		return (Boolean) ret.getValue();
 	}
 
 	@Override
@@ -44,6 +43,5 @@ public class WhileEvaluator extends Evaluator
 			throw new InvalidTypeException(t.getType(), "While");
 		}		
 	}
-
 }
 
