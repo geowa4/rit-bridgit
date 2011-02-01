dffd9c3b29922b732c22c462abde94061fdb5567
G.W.Adams.IV <G.W.Adams.IV@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-01 02:11:44 +0000

    Added tests for mod and minus.

---
+++ trunk/BridgIt/test/edu/rit/se/bridgit/language/evaluator/arithmetic/MinusTest.java
@@ -0,0 +1,96 @@
+package edu.rit.se.bridgit.language.evaluator.arithmetic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.rit.se.bridgit.language.evaluator.Scope;
+import edu.rit.se.bridgit.language.evaluator.term.DoubleEvaluator;
+import edu.rit.se.bridgit.language.evaluator.term.IntegerEvaluator;
+import edu.rit.se.bridgit.language.evaluator.term.StringEvaluator;
+import edu.rit.se.bridgit.language.model.InvalidTypeException;
+import edu.rit.se.bridgit.language.model.NameConflictException;
+import edu.rit.se.bridgit.language.model.Type;
+
+public class MinusTest
+{
+	private Scope scope;
+
+	@Before
+	public void createScope()
+	{
+		scope = new Scope(null);
+	}
+
+	@Test
+	public void integerSubtractedFromIntegerIsAnInteger()
+	throws InvalidTypeException, NameConflictException
+	{
+		MinusEvaluator evaluator = new MinusEvaluator(
+				new IntegerEvaluator(1), new IntegerEvaluator(1));
+		Type ret = evaluator.evaluate(scope);
+		assertEquals("1-1 must be integer 0.", 0, ret.getValue());
+	}
+	
+	@Test
+	public void integerSubtractedFromDoubleIsADouble()
+	throws InvalidTypeException, NameConflictException
+	{
+		MinusEvaluator evaluator = new MinusEvaluator(
+				new IntegerEvaluator(3), new DoubleEvaluator(2.0));
+		Type ret = evaluator.evaluate(scope);
+		assertEquals("3-2.0 must be double 1.0.", 1.0, (Double) ret.getValue(), 0.01);
+	}
+	
+	@Test
+	public void doubleSubtractedFromDoubleIsADouble()
+	throws InvalidTypeException, NameConflictException
+	{
+		MinusEvaluator evaluator = new MinusEvaluator(
+				new DoubleEvaluator(3.0), new DoubleEvaluator(4.0));
+		Type ret = evaluator.evaluate(scope);
+		assertEquals("3.0-4.0 must be double -1.0.", -1.0, (Double) ret.getValue(), 0.01);
+	}
+	
+	@Test
+	public void doubleSubtractedFromIntegerIsADouble()
+	throws InvalidTypeException, NameConflictException
+	{
+		MinusEvaluator evaluator = new MinusEvaluator(
+				new DoubleEvaluator(3.0), new IntegerEvaluator(3));
+		Type ret = evaluator.evaluate(scope);
+		assertEquals("3.0-3 must be double 0.0.", 0.0, (Double) ret.getValue(), 0.01);
+	}
+	
+	@Test(expected=InvalidTypeException.class)
+	public void stringSubtractedFromNumberIsNotAllowed()
+	throws InvalidTypeException, NameConflictException
+	{
+		MinusEvaluator evaluator = new MinusEvaluator(
+				new StringEvaluator("9"), new IntegerEvaluator(3));
+		evaluator.evaluate(scope);
+		fail("A string cannot be used in subtraction.");
+	}
+	
+	@Test(expected=InvalidTypeException.class)
+	public void numberSubtractedFromStringIsNotAllowed()
+	throws InvalidTypeException, NameConflictException
+	{
+		MinusEvaluator evaluator = new MinusEvaluator(
+				new IntegerEvaluator(9), new StringEvaluator("3"));
+		evaluator.evaluate(scope);
+		fail("A string cannot be used in subtraction.");
+	}
+	
+	@Test(expected=InvalidTypeException.class)
+	public void stringSubtractedFromStringIsNotAllowed()
+	throws InvalidTypeException, NameConflictException
+	{
+		MinusEvaluator evaluator = new MinusEvaluator(
+				new StringEvaluator("9"), new StringEvaluator("3"));
+		evaluator.evaluate(scope);
+		fail("A string cannot be used in subtraction.");
+	}
+}
