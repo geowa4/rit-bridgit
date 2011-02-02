ed0f26a95ff0e6babffcc469ff547d43abcc6f11
sankaran.priya05@gmail.com <sankaran.priya05@gmail.com@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-02 01:20:28 +0000

    New Parameter Evaluator class

---
+++ trunk/BridgIt/src/edu/rit/se/bridgit/language/evaluator/ParameterEvaluator.java
@@ -0,0 +1,34 @@
+package edu.rit.se.bridgit.language.evaluator;
+
+import edu.rit.se.bridgit.language.model.InvalidTypeException;
+import edu.rit.se.bridgit.language.model.NameConflictException;
+import edu.rit.se.bridgit.language.model.Type;
+
+public class ParameterEvaluator extends Evaluator{
+
+	
+	private String name;
+	private String type;
+	
+	public ParameterEvaluator(String name, String pseudoType)
+	{
+		this.name = name;
+		this.type = pseudoType;
+	}
+	@Override
+	public Type evaluate(Scope scope) throws InvalidTypeException,
+			NameConflictException {
+		
+		//scope.addParameter(name, new Type(null, type));
+		return new Type(null, type);
+	}
+
+	@Override
+	protected void validateType(Type t) throws InvalidTypeException {
+		if(!t.getType().equals(Boolean.class))
+		{
+			throw new InvalidTypeException(t.getType(), "Parameter");
+		}
+	}
+
+}
