7e4c8262841397ccdf4806f3c2ea788cbfc123e9
sankaran.priya05 <sankaran.priya05@gmail.com>
2011-02-02 01:20:00 +0000

    New Function Call Evaluator

---
+++ BridgIt/src/edu/rit/se/bridgit/language/evaluator/FunctionCallEvaluator.java
@@ -0,0 +1,36 @@
+package edu.rit.se.bridgit.language.evaluator;
+
+import edu.rit.se.bridgit.language.model.InvalidTypeException;
+import edu.rit.se.bridgit.language.model.NameConflictException;
+import edu.rit.se.bridgit.language.model.Type;
+
+public class FunctionCallEvaluator extends Evaluator{
+
+	private String name;
+	private Evaluator arguments;
+	
+	public FunctionCallEvaluator(String name, Evaluator arguments){
+		this.name = name;
+		this.arguments = arguments;
+	}
+	
+	@Override
+	public Type evaluate(Scope scope) throws InvalidTypeException, NameConflictException {
+		
+		if(arguments != null){
+			Type eval = arguments.evaluate(scope);
+			validateType(eval);
+			if(eval.getValue()!= null){
+				return eval;
+			}
+		}	
+		return null;
+	}
+
+	@Override
+	protected void validateType(Type t) throws InvalidTypeException {
+		
+	}
+
+}
+
