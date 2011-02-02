096ce4e5b6e048cfa183588cb4d6ca997da70d42
sankaran.priya05 <sankaran.priya05@gmail.com>
2011-02-02 02:32:11 +0000

    New Function Evaluator class

---
+++ BridgIt/src/edu/rit/se/bridgit/language/evaluator/FunctionEvaluator.java
@@ -0,0 +1,56 @@
+package edu.rit.se.bridgit.language.evaluator;
+
+import edu.rit.se.bridgit.language.model.InvalidTypeException;
+import edu.rit.se.bridgit.language.model.NameConflictException;
+import edu.rit.se.bridgit.language.model.Type;
+
+public class FunctionEvaluator extends Evaluator{
+
+	private String name;
+	private Evaluator parameters;
+	private String returnType;
+	private Evaluator functionBlock;
+	private boolean nestScope;
+	
+	
+	public FunctionEvaluator(String name, Evaluator parameters, boolean nestScope)
+	{
+		this.name = name;
+		this.parameters = parameters;
+		this.nestScope = nestScope;
+	}
+	
+	public FunctionEvaluator(String name, Evaluator parameters, String type, boolean nestScope)
+	{
+		this.name = name;
+		this.parameters = parameters;
+		this.returnType = type;
+		this.nestScope = nestScope;
+	}
+	
+	public void addFunctionBlock(Evaluator functionBlock){
+		this.functionBlock = functionBlock;
+	}
+	
+	@Override
+	public Type evaluate(Scope scope) throws InvalidTypeException,
+			NameConflictException {
+		
+		if(nestScope)
+			scope = new Scope(scope);
+		
+		if(parameters != null){
+			parameters.evaluate(scope);
+		}	
+		if(functionBlock != null){
+			functionBlock.evaluate(scope);
+		}
+		return null;
+	}
+
+	@Override
+	protected void validateType(Type t) throws InvalidTypeException {
+		
+	}
+
+}
