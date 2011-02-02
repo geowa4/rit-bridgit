973aa14e43da628e8156111320290e8584515e60
G.W.Adams.IV <G.W.Adams.IV@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-02 22:18:59 +0000

    It seems like functions need to be shoved off into their own package.

--- BridgIt/src/edu/rit/se/bridgit/language/evaluator/function/GroupEvaluator.java
+++ BridgIt/src/edu/rit/se/bridgit/language/evaluator/function/GroupEvaluator.java
@@ -1,8 +1,10 @@
-package edu.rit.se.bridgit.language.evaluator;
+package edu.rit.se.bridgit.language.evaluator.function;
 
 import java.util.LinkedList;
 import java.util.List;
 
+import edu.rit.se.bridgit.language.evaluator.Evaluator;
+import edu.rit.se.bridgit.language.evaluator.Scope;
 import edu.rit.se.bridgit.language.model.InvalidTypeException;
 import edu.rit.se.bridgit.language.model.NameConflictException;
 import edu.rit.se.bridgit.language.model.Type;
