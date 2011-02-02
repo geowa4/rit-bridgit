f1d9df4e795f0e128e0e8fe7bd1643e250d6996d
sankaran.priya05@gmail.com <sankaran.priya05@gmail.com@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-02 01:17:32 +0000

    New Group Evaluator for parameters and arguments grouping

---
+++ trunk/BridgIt/src/edu/rit/se/bridgit/language/evaluator/GroupEvaluator.java
@@ -0,0 +1,34 @@
+package edu.rit.se.bridgit.language.evaluator;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.rit.se.bridgit.language.model.InvalidTypeException;
+import edu.rit.se.bridgit.language.model.NameConflictException;
+import edu.rit.se.bridgit.language.model.Type;
+
+public class GroupEvaluator extends Evaluator{
+
+	private List<Evaluator> itemList = new LinkedList<Evaluator>();
+	
+	public boolean addItem(Evaluator expression){
+		return itemList.add(expression);
+	}
+	
+	@Override
+	public Type evaluate(Scope scope) throws InvalidTypeException,
+			NameConflictException {
+		for(Evaluator e: itemList){
+			Type eval = e.evaluate(scope);
+			validateType(eval);
+			if(eval.getValue() != null){
+				return eval;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	protected void validateType(Type t) throws InvalidTypeException {}
+
+}
