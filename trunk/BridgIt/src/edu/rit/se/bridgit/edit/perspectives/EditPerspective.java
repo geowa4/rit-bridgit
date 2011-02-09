180d06bc3ef992d170455a7d160180985c9fdb7a
bradley.r.blankenship <bradley.r.blankenship@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-09 19:10:59 +0000

    The editor is now visible. Now, providing nothing crashes, we can test sending text to the parser to execute.

--- trunk/BridgIt/src/edu/rit/se/bridgit/edit/perspectives/EditPerspective.java
+++ trunk/BridgIt/src/edu/rit/se/bridgit/edit/perspectives/EditPerspective.java
@@ -4,10 +4,8 @@ import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class EditPerspective implements IPerspectiveFactory {
-
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
 		layout.setEditorAreaVisible(false);
 	}
-
 }
