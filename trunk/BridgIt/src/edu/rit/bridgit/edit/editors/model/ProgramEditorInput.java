0dbad4edaa05e21dd8835f0e56f046bbd975b085
bradley.r.blankenship <bradley.r.blankenship@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-14 20:43:40 +0000

    Updated the editor to now use the internal text editor component. Need to figure out why it does not allow for the text area to be edited by the user.

--- trunk/BridgIt/src/edu/rit/bridgit/edit/editors/model/ProgramEditorInput.java
+++ trunk/BridgIt/src/edu/rit/bridgit/edit/editors/model/ProgramEditorInput.java
@@ -1,21 +1,23 @@
 package edu.rit.bridgit.edit.editors.model;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IPersistableElement;
 
 public class ProgramEditorInput implements IEditorInput {
-
+	private final String inputString = new String();
+	
 	@Override
 	public Object getAdapter(Class adapter) {
 		// TODO Auto-generated method stub
-		return null;
+		return Platform.getAdapterManager().getAdapter(this, adapter);
 	}
 
 	@Override
 	public boolean exists() {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
--- trunk/BridgIt/src/edu/rit/se/bridgit/edit/editors/ProgramEditor.java
+++ trunk/BridgIt/src/edu/rit/se/bridgit/edit/editors/ProgramEditor.java
@@ -3,14 +3,21 @@ package edu.rit.se.bridgit.edit.editors;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 import edu.rit.bridgit.edit.editors.model.ProgramDocumentProvider;
+import edu.rit.bridgit.edit.editors.model.ProgramEditorInput;
 import edu.rit.bridgit.edit.editors.model.ProgramEditorModel;
 
-public class ProgramEditor extends AbstractDecoratedTextEditor {
+public class ProgramEditor extends AbstractTextEditor {
 	// The editor model of the program
 	ProgramEditorModel m_EditorModel = null;
 	
@@ -25,16 +32,32 @@ public class ProgramEditor extends AbstractDecoratedTextEditor {
 		m_EditorModel = new ProgramEditorModel();
 		this.setDocumentProvider(new ProgramDocumentProvider());
 	}
-	/*
+	
+	@Override
+	public void init(final IEditorSite site, final IEditorInput input) throws PartInitException {
+		super.init(site, input);
+		if(input instanceof ProgramEditorInput) {
+			
+		}
+	}
+	
 	@ Override 
 	public void createPartControl(Composite parent) {
 		super.createPartControl(parent);
-		StyledText text = new StyledText(parent,
-		SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
-		text.setEditable(false);
-		Color bg = Display.getDefault().getSystemColor(SWT.COLOR_GRAY);
-		text.setBackground(bg);
-		text.setText("Here's some text!");
+		
+		// Create and set the layout
+//		FillLayout layout = new FillLayout();
+//		parent.setLayout(layout);
+//		parent.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+//		
+//		// Create the text component and required objects for setup
+//		StyledText text = new StyledText(parent,
+//		SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
+//		Color bg = Display.getDefault().getSystemColor(SWT.COLOR_WHITE);
+//		
+//		// Set the text values
+//		text.setEditable(true);
+//		text.setBackground(bg);
+//		text.setText("");
 	}
-	*/
 }
