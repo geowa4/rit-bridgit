75c8e1c3aefc66a067fc6138effcf881c9d21b9e
sankaran.priya05@gmail.com <sankaran.priya05@gmail.com@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-14 18:53:59 +0000

    Step 1 - check if the function has been defined

--- trunk/BridgIt/src/edu/rit/se/bridgit/language/evaluator/function/FunctionCallEvaluator.java
+++ trunk/BridgIt/src/edu/rit/se/bridgit/language/evaluator/function/FunctionCallEvaluator.java
@@ -1,38 +1,41 @@
 package edu.rit.se.bridgit.language.evaluator.function;
 
 import edu.rit.se.bridgit.language.evaluator.Evaluator;
+
 import edu.rit.se.bridgit.language.evaluator.Scope;
 import edu.rit.se.bridgit.language.model.InvalidTypeException;
 import edu.rit.se.bridgit.language.model.NameConflictException;
 import edu.rit.se.bridgit.language.model.Type;
 
-public class FunctionCallEvaluator extends Evaluator{
+public class FunctionCallEvaluator extends Evaluator {
 
 	private String name;
 	private Evaluator arguments;
-	
-	public FunctionCallEvaluator(String name, Evaluator arguments){
+
+	public FunctionCallEvaluator(String name, Evaluator arguments) {
 		this.name = name;
 		this.arguments = arguments;
 	}
-	
+
 	@Override
-	public Type evaluate(Scope scope) throws InvalidTypeException, NameConflictException {
-		
-		if(arguments != null){
+	public Type evaluate(Scope scope) throws InvalidTypeException,
+			NameConflictException {
+		if (scope.isFunction(name) && arguments != null) {
+			
 			Type eval = arguments.evaluate(scope);
 			validateType(eval);
-			if(eval.getValue()!= null){
+			if (eval.getValue() != null) {
 				return eval;
 			}
-		}	
+		} else {
+			throw new NameConflictException(name, "has not been defined");
+		}
 		return null;
 	}
 
 	@Override
 	protected void validateType(Type t) throws InvalidTypeException {
-		
+
 	}
 
 }
-
