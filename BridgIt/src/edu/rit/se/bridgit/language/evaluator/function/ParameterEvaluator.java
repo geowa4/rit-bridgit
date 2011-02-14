9ae8a164a070cb6c5b709cd8a8d8c9bb82a03822
sankaran.priya05 <sankaran.priya05@gmail.com>
2011-02-14 18:55:47 +0000

    adding parameter to scope

--- BridgIt/src/edu/rit/se/bridgit/language/evaluator/function/ParameterEvaluator.java
+++ BridgIt/src/edu/rit/se/bridgit/language/evaluator/function/ParameterEvaluator.java
@@ -2,6 +2,12 @@ package edu.rit.se.bridgit.language.evaluator.function;
 
 import edu.rit.se.bridgit.language.evaluator.Evaluator;
 import edu.rit.se.bridgit.language.evaluator.Scope;
+import edu.rit.se.bridgit.language.evaluator.VariableEvaluator;
+import edu.rit.se.bridgit.language.evaluator.term.BooleanEvaluator;
+import edu.rit.se.bridgit.language.evaluator.term.DoubleEvaluator;
+import edu.rit.se.bridgit.language.evaluator.term.IntegerEvaluator;
+import edu.rit.se.bridgit.language.evaluator.term.NullEvaluator;
+import edu.rit.se.bridgit.language.evaluator.term.StringEvaluator;
 import edu.rit.se.bridgit.language.model.InvalidTypeException;
 import edu.rit.se.bridgit.language.model.NameConflictException;
 import edu.rit.se.bridgit.language.model.Type;
@@ -20,9 +26,21 @@ public class ParameterEvaluator extends Evaluator{
 	@Override
 	public Type evaluate(Scope scope) throws InvalidTypeException,
 			NameConflictException {
-		
-		//scope.addParameter(name, new Type(null, type));
-		return new Type(null, type);
+		Type eval = new Type(null, type);
+		/*
+		if(eval.getPseudoType().equalsIgnoreCase("Integer"))
+			eval =  new Type(new IntegerEvaluator(0), type);
+		else if(eval.getPseudoType().equalsIgnoreCase("String"))
+			eval = new Type(new StringEvaluator(""), type);
+		else if(eval.getPseudoType().equalsIgnoreCase("Boolean"))
+			eval = new Type(new BooleanEvaluator(false), type);
+		else if(eval.getPseudoType().equalsIgnoreCase("Double"))
+			eval = new Type(new DoubleEvaluator(0.0), type);
+		else 
+			eval = new Type(new NullEvaluator(), type);
+			*/
+		scope.addParameter(name, eval);		
+		return eval;
 	}
 
 	@Override
