e4221b7a676f03e05cf7281afcf262907ca0e62c
bradley.r.blankenship <bradley.r.blankenship@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-02-16 21:44:41 +0000

    Updated the classpath and variables for the plugin.xml to properly include ANTLR.

--- BridgIt/src/edu/rit/se/bridgit/edit/editors/ProgramEditor.java
+++ BridgIt/src/edu/rit/se/bridgit/edit/editors/ProgramEditor.java
@@ -13,12 +13,14 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
 
 import edu.rit.bridgit.edit.editors.model.ProgramDocumentProvider;
-import edu.rit.bridgit.edit.editors.model.ProgramEditorInput;
 import edu.rit.bridgit.edit.editors.model.ProgramEditorModel;
 
 public class ProgramEditor extends AbstractDecoratedTextEditor {
 	// The editor model of the program
 	ProgramEditorModel m_EditorModel = null;
+	
+	// The static styled text that is used for all text input
+	public static StyledText text;
 	
 	/**
 	 * Base constructor
@@ -48,12 +50,13 @@ public class ProgramEditor extends AbstractDecoratedTextEditor {
 		parent.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		// Create the text component and required objects for setup
-		StyledText text = new StyledText(parent,
+		text = new StyledText(parent,
 		SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
 		Color bg = Display.getDefault().getSystemColor(SWT.COLOR_WHITE);
 		
 		// Set the text values
 		text.setEditable(true);
 		text.setBackground(bg);
-		text.setText("");	}
+		text.setText("");
+	}
 }
