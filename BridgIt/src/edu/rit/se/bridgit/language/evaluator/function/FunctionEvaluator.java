31ba8a78c192c139e76e27cbe3246c84f93e711e
sankaran.priya05 <sankaran.priya05@gmail.com>
2011-02-18 20:35:03 +0000

    Changed the function evaluator totally

--- BridgIt/src/edu/rit/se/bridgit/language/evaluator/function/FunctionEvaluator.java
+++ BridgIt/src/edu/rit/se/bridgit/language/evaluator/function/FunctionEvaluator.java
@@ -1,61 +1,69 @@
 package edu.rit.se.bridgit.language.evaluator.function;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import edu.rit.se.bridgit.language.evaluator.Evaluator;
 import edu.rit.se.bridgit.language.evaluator.Scope;
+import edu.rit.se.bridgit.language.evaluator.term.NullEvaluator;
 import edu.rit.se.bridgit.language.model.InvalidTypeException;
 import edu.rit.se.bridgit.language.model.NameConflictException;
 import edu.rit.se.bridgit.language.model.Type;
 
 public class FunctionEvaluator extends Evaluator{
 
-	private String name;
+	
+	private Function function;
 	private Evaluator parameters;
-	private String returnType;
-	private Evaluator functionBlock;
-	private boolean nestScope; 
+	private List<Evaluator> functionBlock = new LinkedList<Evaluator>();
 	
-	
-	public FunctionEvaluator(String name, Evaluator parameters, boolean nestScope)
+	public FunctionEvaluator(String name, Evaluator parameters)
 	{
-		this.name = name;
+		function = new Function();
+		function.setFunctionName(name);
 		this.parameters = parameters;
-		this.nestScope = nestScope;
+
 	}
 	
-	public FunctionEvaluator(String name, Evaluator parameters, String type, boolean nestScope)
+	public FunctionEvaluator(String name, Evaluator parameters, String type)
 	{
-		this.name = name;
+		function = new Function();
+		function.setFunctionName(name);
 		this.parameters = parameters;
-		this.returnType = type;
-		this.nestScope = nestScope;
+		function.setReturnType(type);
+
 	}
 	
-	public void addFunctionBlock(Evaluator functionBlock){
-		this.functionBlock = functionBlock;
+	public void addFunctionBlock(Evaluator e){
+		functionBlock.add(e);
+	}
+	
+	public void addReturnValue(Evaluator e){
+		function.setReturnValue(e);
 	}
 	
 	@Override
 	public Type evaluate(Scope scope) throws InvalidTypeException,
-			NameConflictException {
-		
-		
-		Scope functionScope = null;
-		if(nestScope)
-			functionScope = new Scope(scope);
-		
+			NameConflictException {	
+		List<?> parameter = null;
+		Scope functionScope = new Scope(scope);
+
 		if(parameters != null){
-			parameters.evaluate(functionScope);
-		}	
-		
-			Type type = functionBlock.evaluate(functionScope);
-			scope.addFunction(name, type);
-		
+			parameter = (List<?>) parameters.evaluate(functionScope).getValue();		
+		} 		
+		function.setParameters(parameter);
+		function.setFunctionScope(functionScope);
+		function.setFunctionBlock(functionBlock);
+		Type type = new Type(function, "Function");
+		scope.addFunction(function , type);
 		return type;
 	}
 
 	@Override
 	protected void validateType(Type t) throws InvalidTypeException {
-		
+		if(t.getType() == null || !t.getType().equals(Function.class))
+			throw new InvalidTypeException(t.getType(), "Function");
 	}
 
 }
+
