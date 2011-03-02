46498ce579c7b6ae2aa4c63017167a010082fc93
G.W.Adams.IV <G.W.Adams.IV@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-03-02 22:31:31 +0000

    Added a test and a check on the function to make sure it returned what it said it would.

--- trunk/BridgIt/test/edu/rit/se/bridgit/language/evaluator/function/FunctionTest.java
+++ trunk/BridgIt/test/edu/rit/se/bridgit/language/evaluator/function/FunctionTest.java
@@ -2,6 +2,8 @@ package edu.rit.se.bridgit.language.evaluator.function;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.jmock.Expectations;
 import org.jmock.auto.Mock;
 import org.jmock.integration.junit4.JUnitRuleMockery;
@@ -14,6 +16,7 @@ import edu.rit.se.bridgit.language.evaluator.Evaluator;
 import edu.rit.se.bridgit.language.evaluator.Scope;
 import edu.rit.se.bridgit.language.model.InvalidTypeException;
 import edu.rit.se.bridgit.language.model.NameConflictException;
+import edu.rit.se.bridgit.language.model.Type;
 
 public class FunctionTest
 {
@@ -34,16 +37,42 @@ public class FunctionTest
 		function.setDefinitionScope(scope);
 		function.setFunctionBlock(block);
 		function.setReturnValue(returnExpr);
-		function.setReturnType(Function.VOID_TYPE);
 	}
 	
 	@Test
-	public void functionWithNoReturnIsVoid() throws InvalidTypeException, NameConflictException
+	public void voidFunctionReturnsVoidType() throws InvalidTypeException, NameConflictException
 	{
+		function.setReturnType(Function.VOID_TYPE);
 		context.checking(new Expectations() {{
 			oneOf(block).evaluate(with(any(Scope.class)));
 			never(returnExpr).evaluate(with(any(Scope.class)));
 		}});
 		assertEquals("Type must be void.", Function.VOID_TYPE, function.apply(null).getPseudoType());
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void nonNullParameterListIsEvaluated() throws InvalidTypeException, NameConflictException
+	{
+		function.setReturnType(Function.VOID_TYPE);
+		function.setParameters(params);
+		context.checking(new Expectations() {{
+			oneOf(params).setArgs(with(aNull(List.class)));
+			oneOf(params).evaluate(with(any(Scope.class)));
+			oneOf(block).evaluate(with(any(Scope.class)));
+			never(returnExpr).evaluate(with(any(Scope.class)));
+		}});
+		function.apply(null);
+	}
+	
+	@Test
+	public void typeOfFunctionReturnsSameType() throws InvalidTypeException, NameConflictException
+	{
+		function.setReturnType("Integer");
+		context.checking(new Expectations() {{
+			oneOf(block).evaluate(with(any(Scope.class)));
+			oneOf(returnExpr).evaluate(with(any(Scope.class))); will(returnValue(new Type(1, "Integer")));
+		}});
+		assertEquals("Type should have Pseudo type of \"Integer\".", "Integer", function.apply(null).getPseudoType());
 	}
 }
