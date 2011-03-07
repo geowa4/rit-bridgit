6894af3089d4d3b67f7d75150e5ae605d39c793b
G.W.Adams.IV <G.W.Adams.IV@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-03-07 20:51:46 +0000

    resolves #12 functions are unit tested. all tests pass.

--- trunk/BridgIt/src/edu/rit/se/bridgit/language/model/InvalidTypeException.java
+++ trunk/BridgIt/src/edu/rit/se/bridgit/language/model/InvalidTypeException.java
@@ -3,6 +3,11 @@ package edu.rit.se.bridgit.language.model;
 public class InvalidTypeException extends Exception {
 	private static final long serialVersionUID = -3894386344414091704L;
 	
+	public InvalidTypeException(String text)
+	{
+		super(text);
+	}
+	
 	public InvalidTypeException(Class<?> type, String operation)
 	{
 		super(type + " is an invalid type for " + operation);
---
+++ trunk/BridgIt/test/edu/rit/se/bridgit/language/evaluator/function/ParameterListTest.java
@@ -0,0 +1,84 @@
+package edu.rit.se.bridgit.language.evaluator.function;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.LinkedList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.rit.se.bridgit.language.evaluator.Scope;
+import edu.rit.se.bridgit.language.model.InvalidTypeException;
+import edu.rit.se.bridgit.language.model.NameConflictException;
+import edu.rit.se.bridgit.language.model.Type;
+
+public class ParameterListTest
+{
+	private ParameterListEvaluator ple;
+	private Scope scope;
+	
+	@Before
+	public void createEvaluatorAndScope()
+	{
+		ple = new ParameterListEvaluator();
+		scope = new Scope(null);
+	}
+	
+	@Test(expected=InvalidTypeException.class)
+	public void nullArgsWhenExpectingParamsIsNotAllowed() throws InvalidTypeException, NameConflictException
+	{
+		ple.addParam(new ParameterEvaluator("test", "String"));
+		ple.evaluate(scope);
+		fail("Arguments are null when at least one parameter is expected.");
+	}
+	
+	@Test(expected=InvalidTypeException.class)
+	public void nonNullArgsWhenNotExpectingParamsIsNotAllowed() throws InvalidTypeException, NameConflictException
+	{
+		ple.setArgs(new LinkedList<Type>() {
+			private static final long serialVersionUID = 4920464986871390479L;
+		{
+			add(new Type(null, "Null"));
+		}});
+		ple.evaluate(scope);
+	}
+	
+	@Test(expected=InvalidTypeException.class)
+	public void argsLenAndParamsLenCannotBeDifferent() throws InvalidTypeException, NameConflictException
+	{
+		ple.addParam(new ParameterEvaluator("test", "String"));
+		ple.setArgs(new LinkedList<Type>() {
+			private static final long serialVersionUID = -1267528789248589583L;
+		{
+			add(new Type("hi", "String"));
+			add(new Type("hi", "String"));
+		}});
+		ple.evaluate(scope);
+	}
+	
+	@Test(expected=InvalidTypeException.class)
+	public void pseudoTypesOfArgsAndParamsMustMatch() throws InvalidTypeException, NameConflictException
+	{
+		ple.addParam(new ParameterEvaluator("test", "String"));
+		ple.setArgs(new LinkedList<Type>() {
+			private static final long serialVersionUID = -1267528789248589583L;
+		{
+			add(new Type(1, "Integer"));
+		}});
+		ple.evaluate(scope);
+	}
+	
+	@Test
+	public void parametersAreAddedAsVariablesToScope() throws InvalidTypeException, NameConflictException
+	{
+		ple.addParam(new ParameterEvaluator("test", "String"));
+		ple.setArgs(new LinkedList<Type>() {
+			private static final long serialVersionUID = -1267528789248589583L;
+		{
+			add(new Type("hi", "String"));
+		}});
+		ple.evaluate(scope);
+		assertEquals("", scope.getVariableValue("test").getValue(), "hi");
+	}
+}
