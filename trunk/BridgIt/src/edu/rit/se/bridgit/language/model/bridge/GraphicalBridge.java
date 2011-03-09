be1ee7d0209e6efd29c284e2fed73cdc4200a40c
mxd1365 <mxd1365@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-03-09 18:47:06 +0000

    We now iterate through the content directory and load all obj files along with their associated materials.

--- trunk/BridgIt/src/edu/rit/se/bridgit/language/model/bridge/GraphicalBridge.java
+++ trunk/BridgIt/src/edu/rit/se/bridgit/language/model/bridge/GraphicalBridge.java
@@ -1,24 +1,43 @@
 package edu.rit.se.bridgit.language.model.bridge;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+
+import com.jme.bounding.BoundingBox;
+import com.jme.math.Vector3f;
+import com.jme.renderer.Renderer;
+import com.jme.scene.Node;
 
 public class GraphicalBridge
 {
 	String pseudoType;
 	List<String> availableMethods;
 	Queue<?> actionQueue;
+	Node render_node;
 	
-	public GraphicalBridge(String pseudoType, List<String> methods, Queue<?> actionQueue)
+	public GraphicalBridge(String pseudoType, List<String> methods, Queue<?> actionQueue, Node in_render_node)
 	{
 		this.pseudoType = pseudoType;
 		this.availableMethods = methods;
 		this.actionQueue = actionQueue;
+
+        render_node = in_render_node;
+        
+        render_node.setModelBound(new BoundingBox());
+        render_node.updateModelBound();
+        render_node.setLocalTranslation(new Vector3f(0, 0, -10));
+        render_node.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
+	}
+	
+	public GraphicalBridge(String pseudoType, List<String> methods, Node in_render_node)
+	{
+		this(pseudoType, methods, new LinkedList<String>(), in_render_node);
 	}
 	
 	public GraphicalBridge(GraphicalBridge other) 
 	{
-		this(other.pseudoType, other.availableMethods, other.actionQueue);
+		this(other.pseudoType, other.availableMethods, other.actionQueue, other.render_node);
 	}
 	
 	public Object sendMessage(String methodName) throws NoMethodFoundException
@@ -41,8 +60,13 @@ public class GraphicalBridge
 		return pseudoType;
 	}
 	
-	public String getTumbnail()
+	public String getThumbnail()
 	{
 		return "/Users/student/Desktop/BridgIt_Project/BridgIt/BridgIt/icons/alt_about.gif";
+	}
+	
+	public Node getGeometry()
+	{
+		return render_node;
 	}
 }
--- trunk/BridgIt/src/edu/rit/se/bridgit/language/model/bridge/GraphicalModelBridgeFactory.java
+++ trunk/BridgIt/src/edu/rit/se/bridgit/language/model/bridge/GraphicalModelBridgeFactory.java
@@ -1,22 +1,41 @@
 package edu.rit.se.bridgit.language.model.bridge;
 
+import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Set;
 import java.util.Vector;
 
+import com.jme.renderer.Renderer;
+import com.jme.scene.Node;
+import com.jme.system.DisplaySystem;
+import com.jme.util.export.binary.BinaryImporter;
+import com.jmex.model.converters.FormatConverter;
+import com.jmex.model.converters.ObjToJme;
+
+import edu.rit.se.bridgit.monklypse.RenderCanvas;
+
 public class GraphicalModelBridgeFactory
 {
-	protected static HashMap<String, GraphicalBridge> availableclasses = 
-		new HashMap<String, GraphicalBridge>();;
+	public static HashMap<String, GraphicalBridge> availableclasses = 
+		new HashMap<String, GraphicalBridge>();
 	
-	static 
-	{
-		loadAvailableClasses();
-	}
+	//public static RenderCanvas jme_canvas = new RenderCanvas();
 	
-	protected static HashMap<String, Vector<GraphicalBridge>> currentinstances = 
+	
+	
+	public static HashMap<String, Vector<GraphicalBridge>> currentinstances = 
 		new HashMap<String, Vector<GraphicalBridge>>();
+
+	public static RenderCanvas jme_canvas = new RenderCanvas();
 	
 	public static GraphicalBridge buildBridge(String pseudoType)
 	{
@@ -29,23 +48,99 @@ public class GraphicalModelBridgeFactory
 				currentinstances.put(pseudoType, new Vector<GraphicalBridge>());
 			}
 			currentinstances.get(pseudoType).add(returnval);
+			
+			jme_canvas.addNode(returnval.getGeometry());
 			return returnval;
 		}
 		else
 			return null;
 	}
 	
-	private static void loadAvailableClasses()
+	public static void loadAvailableClasses() throws FileNotFoundException
 	{
-		//TODO this cannot stay like this. we need to actually load something.
-		availableclasses.put("Ballerina", new GraphicalBridge("Ballerina", 
-				new Vector<String>() 
+		
+		File content_folder = new File(System.getProperty("user.home") + File.separator + "Models");
+
+		if(content_folder.exists())
+		{
+			loadModels(content_folder);
+		}
+		else
+		{
+			throw new FileNotFoundException("Content Directory could not be found:\nShould be located at:\n" + content_folder);
+		}
+	}
+	
+	private static void loadModels(File in_current_folder)
+	{
+		File[] list_of_files = in_current_folder.listFiles();
+		for(int i = 0; i < list_of_files.length; i++ )
+		{
+			if(!list_of_files[i].isHidden())
+			{
+				if(list_of_files[i].isFile())
 				{
-					private static final long serialVersionUID = 176187087612072108L;
+					String name = list_of_files[i].getName();
+					if(name.contains(".obj"))
+					{
+						String class_name = list_of_files[i].getName();
+						List<String> methods = new LinkedList<String>();
+						methods.add("jump");
+						methods.add("fly");
+						availableclasses.put(class_name, new GraphicalBridge(class_name, methods, loadObjModel(list_of_files[i])));
+					}
+				}
+				else if(list_of_files[i].isDirectory())
 				{
-					add("jump");
-				}}, 
-				new LinkedList<Object>()));
+					loadModels(list_of_files[i]);
+				}
+			}
+		}
+	}
+	
+	private static Node loadObjModel(File in_model_file)
+	{
+		URL model_url = null;
+		Node model_node = null;
+		try 
+		{
+			model_url = in_model_file.toURI().toURL();
+			
+			// Create something to convert .obj format to .jme
+			FormatConverter converter = new ObjToJme();
+			
+			// Point the converter to where it will find the .mtl file from
+			converter.setProperty("mtllib", model_url);
+			
+			// This byte array will hold my .jme file
+			ByteArrayOutputStream BO = new ByteArrayOutputStream();
+		
+			// Use the format converter to convert .obj to .jme
+			InputStream i_stream = model_url.openStream();
+			converter.convert(i_stream, BO);
+			model_node = (Node) BinaryImporter.getInstance().load(
+				new ByteArrayInputStream(BO.toByteArray()));
+	
+			return model_node;
+		} 
+		catch (MalformedURLException e) 
+		{
+			System.err.println("Could not open file:" + in_model_file.getName());
+
+		}
+		catch (IOException e) 
+		{ 
+			System.err.println("Error converting from obj to jme" + model_url.getFile());
+		}
+		catch (Exception e)
+		{
+			System.err.println("Error in model loading" + model_url.getFile());
+			e.printStackTrace();
+		}
+		
+	
+		return null;
+		
 	}
 
 	public static Set<String> getAvailableClasses()
@@ -65,6 +160,6 @@ public class GraphicalModelBridgeFactory
 	
 	public static String getThumbnailForType(String type)
 	{
-		return ((GraphicalBridge) availableclasses.get(type)).getTumbnail();
+		return ((GraphicalBridge) availableclasses.get(type)).getThumbnail();
 	}
 }
