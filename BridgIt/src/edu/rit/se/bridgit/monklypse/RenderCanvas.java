ec158f495d1beeb754adcbd8d87e374d84681680
mxd1365 <mxd1365@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-03-09 18:47:06 +0000

    We now iterate through the content directory and load all obj files along with their associated materials.

--- BridgIt/src/edu/rit/se/bridgit/monklypse/RenderCanvas.java
+++ BridgIt/src/edu/rit/se/bridgit/monklypse/RenderCanvas.java
@@ -1,37 +1,30 @@
 package edu.rit.se.bridgit.monklypse;
 
-import java.net.URL;
-import java.util.concurrent.Callable;
+import java.io.FileNotFoundException;
 import java.util.logging.Logger;
 
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseTrackListener;
 import org.monklypse.core.SWTDefaultImplementor;
 
-import com.jme.bounding.BoundingBox;
-import com.jme.image.Texture;
 import com.jme.input.FirstPersonHandler;
 import com.jme.input.InputHandler;
 import com.jme.input.KeyInput;
-import com.jme.math.FastMath;
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
-import com.jme.renderer.Renderer;
-import com.jme.scene.shape.Box;
-import com.jme.scene.state.TextureState;
-import com.jme.util.TextureManager;
+import com.jme.scene.Node;
 import com.jmex.swt.input.SWTKeyInput;
 import com.jmex.swt.input.SWTMouseInput;
+
+import edu.rit.se.bridgit.language.model.bridge.GraphicalModelBridgeFactory;
 
 public class RenderCanvas extends SWTDefaultImplementor {
 
         private static final Logger logger = Logger.getLogger(RenderCanvas.class
                         .getName());
 
-        private Quaternion rotQuat;
-        private float angle = 0;
         private Vector3f axis;
-        private Box box;
+        //private Box box;
         long startTime = 0;
         long fps = 0;
         private InputHandler input;
@@ -39,6 +32,7 @@ public class RenderCanvas extends SWTDefaultImplementor {
 
         public RenderCanvas() {
                 super(640, 480);
+                
         }
 
         public void simpleSetup() {
@@ -90,49 +84,88 @@ public class RenderCanvas extends SWTDefaultImplementor {
                 });
                 SWTMouseInput.setup(canvas, true);
                 
-                // Normal Scene setup stuff...
-                rotQuat = new Quaternion();
+                new Quaternion();
                 axis = new Vector3f(1, 1, 0.5f);
                 axis.normalizeLocal();
+                
+                try
+        		{
+        			GraphicalModelBridgeFactory.loadAvailableClasses();
+        		}
+        		catch(FileNotFoundException fnfe)
+        		{
+        			fnfe.printStackTrace();
+        			System.exit(-1);
+        		}
+                
+//                URL model_url = RenderCanvas.class.getClassLoader().getResource("Models" + File.separator + 
+//                		"People" + File.separator + "Ballerina" + File.separator + "Maggie.obj");
+//                
+//                URL mat_url = RenderCanvas.class.getClassLoader().getResource("Models" + File.separator + 
+//                		"People" + File.separator + "Ballerina" + File.separator);
+//                
+//             // Create something to convert .obj format to .jme
+//        		FormatConverter converter = new ObjToJme();
+//        		// Point the converter to where it will find the .mtl file from
+//        		converter.setProperty("mtllib", mat_url);
+//         
+//        		// This byte array will hold my .jme file
+//        		ByteArrayOutputStream BO = new ByteArrayOutputStream();
+//        		try {
+//        			// Use the format converter to convert .obj to .jme
+//        			converter.convert(model_url.openStream(), BO);
+//        			Node maggie = (Node) BinaryImporter.getInstance().load(
+//        				new ByteArrayInputStream(BO.toByteArray()));
+//        			// shrink this baby down some
+//        			maggie.setLocalScale(.07f);
+//        			maggie.setModelBound(new BoundingSphere());
+//        			maggie.updateModelBound();
+//        			// Put her on the scene graph
+//        			rootNode.attachChild(maggie);
+//        		} catch (IOException e) { // Just in case anything happens
+//        			logger.logp(Level.SEVERE, this.getClass().toString(),
+//        				"simpleInitGame()", "Exception", e);
+//        			System.exit(0);
+//        		}
 
-                Vector3f max = new Vector3f(5, 5, 5);
-                Vector3f min = new Vector3f(-5, -5, -5);
+                //Vector3f max = new Vector3f(5, 5, 5);
+                //Vector3f min = new Vector3f(-5, -5, -5);
+//
+//                box = new Box("Box", min, max);
+//                box.setModelBound(new BoundingBox());
+//                box.updateModelBound();
+//                box.setLocalTranslation(new Vector3f(0, 0, -10));
+//                box.setRenderQueueMode(Renderer.QUEUE_SKIP);
+//                rootNode.attachChild(box);
 
-                box = new Box("Box", min, max);
-                box.setModelBound(new BoundingBox());
-                box.updateModelBound();
-                box.setLocalTranslation(new Vector3f(0, 0, -10));
-                box.setRenderQueueMode(Renderer.QUEUE_SKIP);
-                rootNode.attachChild(box);
-
-                box.setRandomColors();
+ //               box.setRandomColors();
 
                 startTime = System.currentTimeMillis() + 5000;
 
                 input = new FirstPersonHandler(cam, 50, 1);
                 //execute in rendercontext to correctly create the resource in the opengl context
-                render(new Callable<Object>() {
+                //render(new Callable<Object>() {
                         /*
                          * (non-Javadoc)
                          * 
                          * @see java.util.concurrent.Callable#call()
                          */
-                        @Override
-                        public Object call() throws Exception {
-                                TextureManager.clearCache();
-                                TextureState ts = renderer.createTextureState();
-                                ts.setEnabled(true);
-                                URL resource = getClass().getClassLoader().getResource(
-                                                "org/monklypse/core/Monkey.jpg");
-                                Texture texture = TextureManager.loadTexture(resource,
-                                                Texture.MinificationFilter.BilinearNearestMipMap,
-                                                Texture.MagnificationFilter.Bilinear);
-                                ts.setTexture(texture);
-                                rootNode.setRenderState(ts);
-                                rootNode.updateRenderState();
-                                return null;
-                        }
-                });
+                        //@Override
+                        //public Object call() throws Exception {
+                                //TextureManager.clearCache();
+                                //TextureState ts = renderer.createTextureState();
+                                //ts.setEnabled(true);
+                                //URL resource = getClass().getClassLoader().getResource(
+                                  //              "org/monklypse/core/Monkey.jpg");
+                                //Texture texture = TextureManager.loadTexture(resource,
+                                  //              Texture.MinificationFilter.BilinearNearestMipMap,
+                                    //            Texture.MagnificationFilter.Bilinear);
+                                //ts.setTexture(texture);
+                                //rootNode.setRenderState(ts);
+                                //rootNode.updateRenderState();
+                                //return null;
+                        //}
+                //});
 
         }
 
@@ -142,27 +175,34 @@ public class RenderCanvas extends SWTDefaultImplementor {
                 }
 
                 // Code for rotating the box... no surprises here.
-                if (tpf < 1) {
-                        angle = angle + (tpf * 25);
-                        if (angle > 360) {
-                                angle = 0;
-                        }
-                }
-                rotQuat.fromAngleNormalAxis(angle * FastMath.DEG_TO_RAD, axis);
-                box.setLocalRotation(rotQuat);
+//                if (tpf < 1) {
+//                        angle = angle + (tpf * 25);
+//                        if (angle > 360) {
+//                                angle = 0;
+//                        }
+//                }
+                //rotQuat.fromAngleNormalAxis(angle * FastMath.DEG_TO_RAD, axis);
+                //box.setLocalRotation(rotQuat);
 
-                if (startTime > System.currentTimeMillis()) {
-                        fps++;
-                } else {
-                        long timeUsed = 5000 + (startTime - System.currentTimeMillis());
-                        startTime = System.currentTimeMillis() + 5000;
-                        doUpdate();
-                        logger.info(fps + " frames in " + (timeUsed / 1000f)
-                                        + " seconds = " + (fps / (timeUsed / 1000f))
-                                        + " FPS (average)");
-                        fps = 0;
-                }
+//                if (startTime > System.currentTimeMillis()) {
+//                        fps++;
+//                } else {
+//                        long timeUsed = 5000 + (startTime - System.currentTimeMillis());
+//                        startTime = System.currentTimeMillis() + 5000;
+//                        doUpdate();
+//                        logger.info(fps + " frames in " + (timeUsed / 1000f)
+//                                        + " seconds = " + (fps / (timeUsed / 1000f))
+//                                        + " FPS (average)");
+//                        fps = 0;
+//                }
 
+        }
+        
+        public void addNode(Node in_model)
+        {
+        	 
+             if(rootNode != null)
+            	 rootNode.attachChild(in_model);
         }
 
         /* (non-Javadoc)
--- BridgIt/src/edu/rit/se/bridgit/views/WorldView.java
+++ BridgIt/src/edu/rit/se/bridgit/views/WorldView.java
@@ -1,19 +1,17 @@
 package edu.rit.se.bridgit.views;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.ViewPart;
 import org.monklypse.core.JMEComposite;
 
+
 import edu.rit.bridgit.edit.editors.model.ProgramEditorInput;
-import edu.rit.se.bridgit.monklypse.RenderCanvas;
+import edu.rit.se.bridgit.language.model.bridge.GraphicalModelBridgeFactory;
 
 public class WorldView extends ViewPart {
 
@@ -25,20 +23,20 @@ public class WorldView extends ViewPart {
 	public void createPartControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.None);
         container.setLayout(new GridLayout(1, true));
-        //for (int i = 0; i < 4; ++i) {
-                JMEComposite composite = new JMEComposite(container,
-                                new RenderCanvas());
-                composite.setLayoutData(new GridData(GridData.FILL_BOTH));
-        //}
-
-             // Attempt to attach the program editor to this view
-        		try {
-        			
-        			IEditorPart editor = getSite().getPage().openEditor(new ProgramEditorInput(),
-        					"edu.rit.se.bridgit.edit.editors.programeditor");
-        		} catch (PartInitException e) {
-        			e.printStackTrace();
-        		}
+        
+        JMEComposite composite = new JMEComposite(container,
+                       GraphicalModelBridgeFactory.jme_canvas);
+        composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+        
+        GraphicalModelBridgeFactory.buildBridge("Ballerina");
+     
+         // Attempt to attach the program editor to this view
+		try {
+			IEditorPart editor = getSite().getPage().openEditor(new ProgramEditorInput(),
+					"edu.rit.se.bridgit.edit.editors.programeditor");
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
