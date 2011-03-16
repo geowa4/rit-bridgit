3b8d5324d30a63092859912e58790f8f0d99ffd8
bradley.r.blankenship <bradley.r.blankenship@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-03-16 15:45:18 +0000

    Added exception handling to insure the caret is still moved correctly, even if parsing fails.

--- BridgIt/src/edu/rit/se/bridgit/edit/editors/EditScopeParser.java
+++ BridgIt/src/edu/rit/se/bridgit/edit/editors/EditScopeParser.java
@@ -18,55 +18,47 @@ public class EditScopeParser
 		// The return value
 		ArrayList<ArrayList<String>> retVal = new ArrayList<ArrayList<String>>();
 
-		// Attempt to accomplish the following
-		try
-		{
-			// Clear out comments
-			p_ProgramText = p_ProgramText.replaceAll("//.*\n*", "");
-			p_ProgramText = p_ProgramText.replaceAll("/\\*(.|\\s)*?\\*/", "");
+		// Clear out comments
+		p_ProgramText = p_ProgramText.replaceAll("//.*\n*", "");
+		p_ProgramText = p_ProgramText.replaceAll("/\\*(.|\\s)*?\\*/", "");
 
-			// Declare the pattern and matcher for regex parsing
-			Pattern variablePattern = Pattern.compile("\\s*var\\s+\\w*\\s*:\\s*\\w*\\s*");
-			Pattern constantPattern = Pattern.compile("\\s*constant\\s+\\w*\\s*:\\s*\\w*\\s*");
-			Pattern functionPattern = Pattern.compile("\\s*function\\s+\\w*\\s*(.*)\\s*:\\s*\\w*\\s*");
+		// Declare the pattern and matcher for regex parsing
+		Pattern variablePattern = Pattern
+				.compile("\\s*var\\s+\\w*\\s*:\\s*\\w*\\s*");
+		Pattern constantPattern = Pattern
+				.compile("\\s*constant\\s+\\w*\\s*:\\s*\\w*\\s*");
+		Pattern functionPattern = Pattern
+				.compile("\\s*function\\s+\\w*\\s*(.*)\\s*:\\s*\\w*\\s*");
 
-			// Declare the matches for each
-			Matcher variableMatcher = variablePattern.matcher(p_ProgramText);
-			Matcher constantMatcher = constantPattern.matcher(p_ProgramText);
-			Matcher functionMatcher = functionPattern.matcher(p_ProgramText);
+		// Declare the matches for each
+		Matcher variableMatcher = variablePattern.matcher(p_ProgramText);
+		Matcher constantMatcher = constantPattern.matcher(p_ProgramText);
+		Matcher functionMatcher = functionPattern.matcher(p_ProgramText);
 
-			// The lists for the values
-			ArrayList<String> variables = new ArrayList<String>();
-			ArrayList<String> constants = new ArrayList<String>();
-			ArrayList<String> functions = new ArrayList<String>();
+		// The lists for the values
+		ArrayList<String> variables = new ArrayList<String>();
+		ArrayList<String> constants = new ArrayList<String>();
+		ArrayList<String> functions = new ArrayList<String>();
 
-			// Add all the variables
-			while (variableMatcher.find())
-					variables.add(variableMatcher.group()
-							.replaceAll("\\s*var\\s*", "").trim());
+		// Add all the variables
+		while (variableMatcher.find())
+			variables.add(variableMatcher.group().replaceAll("\\s*var\\s*", "")
+					.trim());
 
-			// Add all the constants
-			while (constantMatcher.find())
-					constants.add(constantMatcher.group()
-							.replaceAll("\\s*constant\\s*", "").trim());
+		// Add all the constants
+		while (constantMatcher.find())
+			constants.add(constantMatcher.group()
+					.replaceAll("\\s*constant\\s*", "").trim());
 
-			// Add all the functions
-			while (functionMatcher.find())
-					functions.add(functionMatcher.group()
-							.replaceAll("\\s*function\\s*", "").trim());
+		// Add all the functions
+		while (functionMatcher.find())
+			functions.add(functionMatcher.group()
+					.replaceAll("\\s*function\\s*", "").trim());
 
-			// Add the values
-			retVal.add(variables);
-			retVal.add(constants);
-			retVal.add(functions);
-		}
-		// An error occurred
-		catch (Exception e)
-		{
-			// Print out the error
-			System.err.println("Failed scope parsing of program text editor. See the following: ");
-			e.printStackTrace();
-		}
+		// Add the values
+		retVal.add(variables);
+		retVal.add(constants);
+		retVal.add(functions);
 
 		// Return
 		return retVal;
