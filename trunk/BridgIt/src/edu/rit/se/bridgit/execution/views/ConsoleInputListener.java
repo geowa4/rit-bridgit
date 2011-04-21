df8835601ebae926acb1033295b7a2d770827330
G.W.Adams.IV <G.W.Adams.IV@4c3182e4-22b4-3764-e82d-f0a6e243f2a8>
2011-04-21 23:38:24 +0000

    resolves #95 I abandoned the text input area in the Console view in favor of a modal dialog. This solution is just so much easier. And it works. The user calls the built-in input function, passing a string as an argument and it returns the text entered by the user or the empty string if cancel was pressed. Example: myString = input("sample prompt:");

--- trunk/BridgIt/src/edu/rit/se/bridgit/execution/views/ConsoleInputListener.java
+++
@@ -1,9 +0,0 @@
-package edu.rit.se.bridgit.execution.views;
-
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Text;
-
-public interface ConsoleInputListener extends Listener
-{
-	public void setTextInputElement(Text inputElement);
-}
